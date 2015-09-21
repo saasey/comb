a5f854275c96afa7af9369e38e1a27619154ced3
Anthony David Pulse, Jr. <4723286.ap@gmail.com>
2015-09-21 11:12:49 -0400

    Update Comb.cpp

--- Comb.cpp
+++ Comb.cpp
@@ -9,7 +9,7 @@
 using namespace std;
  
 int main(int x, char ** argc, char * argv[]) {
-   char incr[4097];
+   char incr[33];
    int Y=1;
   for (int i=0;i<=x;i++) {
    if (strcmp("-d",argv[i])) {
@@ -17,17 +17,22 @@ int main(int x, char ** argc, char * argv[]) {
      std::ofstream  out(argv[i+2], std::ios::out | std::ios::binary);
    while (!(in.eof())) {
         char P=in.get();
-        char ha[33]= { P };
-       for (int j=0;j<=31;j++) {
-           if (ha[j]==0) {
-               out << (char)Y-1 ;
+        char heh[33]= { P };
+    for (int j=0;j<=31;j++) {
+       if (heh[j]==0) {
+              
+          if (Y%2!=0)
+             Y++;
+               char ha[4]=(char)Y;
+               int X=(ha[x+2]+1)*32+((ha[x]*10)+ha[x+1]);
+               out << (char)X;
                Y=1;
-           }
-           else if (ha[j]==1) {
+           } else if (heh[j]==1) {
+               Y++;
                Y++;
                continue;
            }
-           else
+           else {
               break;
            }
        }
@@ -40,14 +45,35 @@ int main(int x, char ** argc, char * argv[]) {
    std::ifstream  in(argv[i+1]);
    std::ofstream  out(argv[i+2]);
 	unsigned int p=0;
-        int X=1;
+        int X=1,z, C;
 	while (!(in.eof())) {
          if (X==1)
             Y=in.get();
          else
             X=1;
-       if (0xffffffff > sizeof(incr)+(int)Y+1) {
-            for (int j=0;j<=265;j++) {
+
+                 if ((int)Y<32)
+                    z=1;
+                 else if ((int)Y>32)
+                    z=2;
+                 else if ((int)Y>=64)
+                    z=3;
+                 else if ((int)Y>=96)
+                    z=4;
+                 else if ((int)Y>=128)
+                    z=5;
+                 else if ((int)Y>=160)
+                    z=6;
+                 else if ((int)Y>=192)
+                    z=7;
+                 else if ((int)Y>=224)
+                    z=8;
+                 else if ((int)Y==256)
+                    z=9;
+                    C=(int)(Y-((z-1)*32))*10+z-1;
+                 p+=3;
+         if (0xffffffff > sizeof(incr)+(int)C) {
+            for (int j=0;j<=Y;j++) {
                  if ((j) != (int)Y) {
                     int F=1;
                     incr[p++] = F;
@@ -57,13 +83,13 @@ int main(int x, char ** argc, char * argv[]) {
                     break;
                 }
            }
-     } else {
+         } else {
              out << std::hex << incr;
              delete [] incr;
-             char incr[4098];
+             char incr[33];
              X=0;
-      }
-   }
+           }
+     }
      out.close();
      in.close();
      delete [] incr;
