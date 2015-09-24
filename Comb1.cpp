cbcbd681524ced51940083830ac077488ebdb0be
Anthony David Pulse, Jr. <4723286.ap@gmail.com>
2015-09-24 11:19:45 -0400

    Update Comb1.cpp

--- Comb1.cpp
+++ Comb1.cpp
@@ -22,10 +22,14 @@ int main(int x, char ** argc, char * argv[]) {
     for (int j=0;j<=31;j++) {
        if (heh[j]==0) {
           Y+=j*31;
-          P[0]=heh[j+1]+heh[j+2]+heh[j+3]+heh[j+4];
+          P=heh[j+1]+heh[j+2]+heh[j+3]+heh[j+4];
           j+=5;
-          Y+=(int)P;
-          out << (char)Y-1;
+          Y+=P;
+          int G=round(Y/100-0.5)+1;
+          int H=round(Y/10-0.5)+G*10;
+          int I=Y-G-H+2;
+          Y=Y+H*32+(G)+I*2+1;
+          out << (char)Y;
           Y=0;
        } else if (heh[j]==1) {
           Y+=31;
@@ -40,17 +44,23 @@ int main(int x, char ** argc, char * argv[]) {
      cout << "The depression is fiñ";
      return 0;
   } else if (strcmp("-c",argv[i])) {
+    int D=0;
    std::ifstream  in(argv[i+1], std::ios::in | std::ios::binary);
    std::ofstream  out(argv[i+2], std::ios::in | std::ios::dec);
 	unsigned int p=0;
         int X=1;
-	while (!(in.eof())) {
+     while (!(in.eof())) {
          if (X==1)
             Y=in.get();
          else
             X=1;
-
-         if (LLONG_MAX > (int)incr+(int)Y/31+5) {
+         int D=0;
+         for (int i=0;i<=10;i++)
+             if (i*D<(int)Y)
+               D+=31;
+             else if
+               break;
+         if (0xffffffff > (int)incr+(int)Y/31+5) {
                       if (D+1==Y)
                          incr[D]=0000;
                       else if (D+2==Y)
@@ -81,16 +91,36 @@ int main(int x, char ** argc, char * argv[]) {
                          incr[D]=1011;
                       else if (D+15==Y)
                          incr[D]=0111;
-                      else if (D+16==Y)
-                         incr[D]=1111;
                       else if (D==Y)
                          incr[D]=0;
-                      else if (D!=Y)
+                      else if (Y-D>31)
                          incr[D]=1;
                     break;
                 }
+
+         if (incr < 0xffffffff - (int)Y) {
+                 if ((int)Y<32)
+                    z=1;
+                 else if ((int)Y>=32)
+                    z=2;
+                 else if ((int)Y>=64)
+                    z=3;
+                 else if ((int)Y>=96)
+                    z=4;
+                 else if ((int)Y>=128)
+                    z=5;
+                 else if ((int)Y>=160)
+                    z=6;
+                 else if ((int)Y>=192)
+                    z=7;
+                 else if ((int)Y>=224)
+                    z=8;
+                 else if ((int)Y==256)
+                    z=9;
+                    incr[D]=(int)(Y-((z-1)*32))*10+z-1;
+                 p+=3;
          } else {
-             out << std::dec << incr << endl;
+             out << std::hex << incr << endl;
              char incr[63]={};
              X=0;
            }
