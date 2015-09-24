0502845d3e7b800b9f4c7bf508c41a24a7c8ba29
Anthony David Pulse, Jr. <4723286.ap@gmail.com>
2015-09-24 18:17:46 -0400

    Update Comb.cpp

--- Comb.cpp
+++ Comb.cpp
@@ -12,6 +12,8 @@ using namespace std;
 int main(int x, char ** argc, char * argv[]) {
    char incr[33]={ "\0" };
    int Y=32;
+   int indr[33]={ "\0" };
+   int Nmbrcnt=0, x=0;
   for (int i=0;i<=x;i++) {
    if (strcmp("-d",argv[i])) {
      std::ifstream  in(argv[i+1], std::ios::in | std::ios::binary);
@@ -21,13 +23,18 @@ int main(int x, char ** argc, char * argv[]) {
         char P[33] =in.getline();
         char heh[33] = in.getline();
     for (int j=0;j<=31;j++) {
-       if (heh[j]==0) {
-          if (heh[j+1]==0)
-            Y+=heh[j+1]+heh[j+2]+heh[j+3]+heh[j+4]+heh[j+5]+heh[j+6]+heh[j+7]+heh[j+8];
-          else
+       if (P[V]==0) {
+          if (P[V+1]==0) {
             Y+=heh[j+1]+heh[j+2]+heh[j+3]+heh[j+4];
-          j
-          out << (char)Y-1;
+            Y+=heh[j+5]+heh[j+6]+heh[j+7]+heh[j+8];
+            j+=9;
+            V++;
+          }
+          else {
+            Y+=heh[j+1]+heh[j+2]+heh[j+3]+heh[j+4];
+          }
+          V++;
+          out << (char)Y;
           Y=0;
        } else if (heh[j]==1) {
           Y+=32;
@@ -47,70 +54,82 @@ int main(int x, char ** argc, char * argv[]) {
 	unsigned int p=0;
         int X=1;
 	while (!(in.eof())) {
-         if (X==1)
+         int a=0;
+         int D=0;
+         if (q==1) {
             Y=in.get();
-            if ((int)Y>=
-         else
-            X=1;
-         while (a) {
-         if (32 >= incr.c_str(),length()) {
-                      if (D+1==Y)
+            if ((int)Y%32<1)
+               int Nmbrcnt = Y/32;
+            else
+               x=Y%32;
+         }
+         else {
+            q=1;
+         }
+        for (int j=0;j<=Nmbrcnt-1;j++)
+            indr[j]=1;
+        if (x<16)
+           a=1;
+        indr[Nmbrcnt]=0;
+        while (x>0) {
+         if (28 >= incr.c_str().length()) {
+           while (a>=2) {
+                      if (1==Y)
                          incr[D]=0000;
-                      else if (D+2==Y)
+                      else if (2==Y)
                          incr[D]=1000;
-                      else if (D+3==Y)
+                      else if (3==Y)
                          incr[D]=0100;
-                      else if (D+4==Y)
+                      else if (4==Y)
                          incr[D]=1100;
-                      else if (D+5==Y)
+                      else if (5==Y)
                          incr[D]=0010;
-                      else if (D+6==Y)
+                      else if (6==Y)
                          incr[D]=1010;
-                      else if (D+7==Y)
+                      else if (7==Y)
                          incr[D]=0110;
-                      else if (D+8==Y)
+                      else if (8==Y)
                          incr[D]=1110;
-                      else if (D+9==Y)
+                      else if (9==Y)
                          incr[D]=0001;
-                      else if (D+10==Y)
+                      else if (10==Y)
                          incr[D]=1001;
-                      else if (D+11==Y)
+                      else if (11==Y)
                          incr[D]=0101;
-                      else if (D+12==Y)
+                      else if (12==Y)
                          incr[D]=1101;
-                      else if (D+13==Y)
+                      else if (13==Y)
                          incr[D]=0011;
-                      else if (D+14==Y)
+                      else if (14==Y)
                          incr[D]=1011;
-                      else if (D+15==Y)
+                      else if (15==Y)
                          incr[D]=0111;
-                      else if (D+16==Y)
+                      else if (16==Y)
                          incr[D]=1111;
-                      else if (D==Y)
-                         incr[D]=0;
-                      else if (D!=Y)
-                         incr[D]=1;
                     break;
-                }
+                 a++;
+           }
          } else {
              out << std::hex << incr << endl;
+             out << std::hex << indr << endl;
+             char indr={};
              char incr={};
              X=0;
            }
+         x-=16;
+       }
      }
      out.close();
      in.close();
-     delete [] incr;
      cout << "The pressing is finished";
      return 0;
  } else if (i==x) {
        cout << "decompress:\t press -d origin.prs out.file" << endl;
        cout << "compress: \t press -c origin.file out.prs" << endl;
        return 0;
- }
+   }
  
 }
-     delete [] incr;
  
      return 0;
 }
